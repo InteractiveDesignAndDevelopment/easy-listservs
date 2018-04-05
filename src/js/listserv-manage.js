c879ad2e6878f0f50039403033443b3efa6a3bf2
Todd Sayre <captain.friendly@gmail.com>
2018-04-05 13:06:49 -0400

    JS now all works

--- src/js/listserv-manage.js
+++ src/js/listserv-manage.js
@@ -1,49 +1,40 @@
-// Element.closest polyfill
-if (window.Element && !Element.prototype.closest) {
-    Element.prototype.closest =
-        function (s) {
-            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-                i,
-                el = this;
-            do {
-                i = matches.length;
-                while (--i >= 0 && matches.item(i) !== el) {};
-            } while ((i < 0) && (el = el.parentElement));
-            return el;
-        };
-}
-
-/*
+/**
+ * @author Todd Sayre
+ *
  * Email Template From Original Form Handler
+ *
  * \\hubbabubba\e$\Inetpub\wwwroot\forms\subscribe2list.cfm
+ *
  * To: `mailserv@mercer.edu`
  * Subject: `<Subscribe|Unsubscribe> <listserv id>`
  * Body: `<Subscribe|Unsubscribe> <listserv id>`
+ *
  */
+
+require('element-closest');
+require('nodelist-foreach');
+
 document.addEventListener('DOMContentLoaded', function () {
-    // console.log('Ready!');
 
-    var emailTo = 'mailserv@mercer.edu';
-    var unSubScribeButtons = document.querySelectorAll('.un-sub-scribe');
+    let toAddress = 'mailserv@mercer.edu';
+    let actionButtons = document.querySelectorAll('.listserv-control__action');
 
-    Array.prototype.forEach.call(unSubScribeButtons, function (link) {
-        var div = link.closest('[data-list-id]');
-        var listservId = div.getAttribute('data-list-id');
-        var href = 'mailto:' + emailTo;
-        var isSubscribe = !/unsub/.test(link.innerText);
-        var command = '';
+    actionButtons.forEach(link => {
+        let listservContainer = link.closest('[data-list-id]');
+        let listservId = listservContainer.getAttribute('data-list-id');
+        let isSubscribe = ! /unsubscribe/.test(link.className);
+        let command = 'Subscribe';
 
-        if (isSubscribe) {
-            command += 'Subscribe ';
-        } else {
-            command += 'Unsubscribe ';
+        if (! isSubscribe) {
+            command = 'Unsubscribe';
         }
 
-        command += listservId;
+        let body = `${command} ${listservId}`;
 
-        href += '?subject=' + command + '&body=' + command;
+        let hrefValue = `mailto:${toAddress}?subject=${body}&body=${body}`;
+        hrefValue = hrefValue.replace(' ', '%20');
 
-        link.setAttribute('href', href);
+        link.setAttribute('href', hrefValue);
     });
 
 });
