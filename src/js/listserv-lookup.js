0ddb6277eec89cc309d6e2977af4ae1fc38a5fa8
Todd Sayre <sayre_ts@mercer.edu>
2018-04-05 16:55:15 -0400

    Clear course field when generate button is clicked
    The course field clears out when the generate button is clicked in case the visitor wants to use the form again.

--- src/js/listserv-lookup.js
+++ src/js/listserv-lookup.js
@@ -7,13 +7,13 @@
  * ```
  */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   let courseField = document.getElementById('course');
   let emailButton = document.getElementById('email');
 
   let mailTo = emailButton.getAttribute('href');
 
-  courseField.addEventListener('keyup', function() {
+  courseField.addEventListener('keyup', () => {
     let course = courseField.value;
     let body = `DIR/LIST ${course}*`;
     let newMailTo = `${mailTo}?subject=${body}&body=${body}`;
@@ -24,5 +24,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // console.log(newMailTo);
 
     emailButton.setAttribute('href', newMailTo);
+  });
+
+  emailButton.addEventListener('click', () => {
+    courseField.value = '';
   });
 });
